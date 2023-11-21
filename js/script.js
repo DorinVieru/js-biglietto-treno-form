3074dcffbb1f3f3a7a981d9710d5f9fa3beab8bb
DorinVieru <dorino.vieru@gmail.com>
2023-11-21 18:01:40 +0100

    [UPDATE] Improve js code % finish project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // PREZZO BIGLIETTO
 let cost = 0.21;
+console.log(cost);
 
 let price;
 
@@ -11,6 +12,9 @@ button_genera.addEventListener("click", function(){
     let nome_e_cognome = document.getElementById("nome_e_cognome").value;
     let km = document.getElementById("km").value;
     let eta = document.getElementById("eta").value;
+    let offerta;
+    let carrozza;
+    let codice_cp;
 
     console.log(nome_e_cognome);
     console.log(km);
@@ -20,33 +24,33 @@ button_genera.addEventListener("click", function(){
     if (eta == "min") {
         let sconto = cost * 0.2 * km;
         price = cost * km - sconto;
-        document.getElementById("nome").innerText = nome_e_cognome;
-        document.getElementById("offerta").innerText = "Biglietto Sconto Minori";
-        document.getElementById("carrozza").innerText = 2;
-        document.getElementById("codice_cp").innerText = 92800;
-        document.getElementById("costo_biglietto").innerText = `${price} \u20ac`;
+        offerta = "Biglietto Sconto Minori";
+        carrozza = 2;
+        codice_cp = 92800;
     }
 
     else if (eta == "over") {
         sconto = cost * 0.4 * km;
         price = cost * km - sconto;
-        document.getElementById("nome").innerText = nome_e_cognome;
-        document.getElementById("offerta").innerText = "Biglietto Sconto Over 65";
-        document.getElementById("carrozza").innerText = 7;
-        document.getElementById("codice_cp").innerText = 92855;
-        document.getElementById("costo_biglietto").innerText = `${price} \u20ac`;
+        offerta = "Biglietto Sconto Over 65";
+        carrozza = 7;
+        codice_cp = 92850;
     }
 
     else {
         price = cost * km;
-        document.getElementById("nome").innerText = nome_e_cognome;
-        document.getElementById("offerta").innerText = "Biglietto Standard";
-        document.getElementById("carrozza").innerText = 5;
-        document.getElementById("codice_cp").innerText = 92911;
-        document.getElementById("costo_biglietto").innerText = `${price} \u20ac`;
+        offerta = "Biglietto Sconto Standard";
+        carrozza = 5;
+        codice_cp = 92911;
     }
 
     // PREZZO DEL BIGLIETTO IN CONSOLE.LOG
     price = price.toFixed(2);
     console.log(price);
+
+    document.getElementById("nome").innerText = nome_e_cognome;
+    document.getElementById("offerta").innerText = offerta;
+    document.getElementById("carrozza").innerText = carrozza;
+    document.getElementById("codice_cp").innerText = codice_cp;
+    document.getElementById("costo_biglietto").innerText = `${price} \u20ac`;
 })
