5ac88fb7edc57bd7cbef01e084e19ddc5ce7ebef
DorinVieru <dorino.vieru@gmail.com>
2023-11-21 16:11:52 +0100

    [UPDATE] Price calculation on js with console.log

--- js/script.js
+++ js/script.js
@@ -15,4 +15,24 @@ button_genera.addEventListener("click", function(){
     console.log(nome_e_cognome);
     console.log(km);
     console.log(eta);
+
+    // CALCOLO DEL PREZZO
+    if (eta == "min") {
+        let sconto = cost * 0.2 * km;
+        price = cost * km - sconto;
+
+    }
+
+    else if (eta == "over") {
+        sconto = cost * 0.4 * km;
+        price = cost * km - sconto;
+    }
+
+    else {
+        price = cost * km;
+    }
+
+    // PREZZO DEL BIGLIETTO IN CONSOLE.LOG
+    price = price.toFixed(2);
+    console.log(price);
 })
